71fc06456f51f245507b641f43d1d394454598bc
Bziluch <bziluch@gmail.com>
2024-07-22 22:23:08 +0200

    feat: adding articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -20,6 +20,12 @@ class ArticleController extends SecureController
     public function list()
     {
         $articles = $this->articleRepository->findAll();
+
+        if (isset($_POST['title']) && isset($_POST['description'])) {
+            $this->articleRepository->addArticle($_POST['title'], $_POST['description']);
+            $this->redirect('/');
+        }
+
         $this->renderView('articles/list.twig', [
             'articles' => $articles
         ]);
--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -14,4 +14,11 @@ class ArticleRepository extends AbstractRepository
         $stmt->execute();
         return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
     }
+
+    public function addArticle(string $title, string $description): void
+    {
+        $stmt = $this->dbManager->getMysqli()->prepare('INSERT INTO article(title, description) VALUES (?,?)');
+        $stmt->bind_param('ss', $title, $description);
+        $stmt->execute();
+    }
 }
